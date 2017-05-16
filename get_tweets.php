895ea6a362553874ef988a8fe7c7492e52c494c7
James H-Hall <james.hall@impression.co.uk>
2017-05-16 10:13:07 +0100

    Exclude replies for now

--- get_tweets.php
+++ get_tweets.php
@@ -6,22 +6,26 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 if($options = get_option( 'swptc_settings' )){
 
   $transient = get_transient( 'swptc_latest_tweets' );
-  if ( empty( $transient ) ){
+  if ( 1 ){
     $connection = new TwitterOAuth($option['consumer_key'], $option['consumer_secret'], $option['access_token'], $option['access_token_secret']);
     $content = $connection->get("account/verify_credentials");
-    $statuses = $connection->get("statuses/user_timeline", ["screen_name" => $option['twitter_handle'], "count" => $option['num_tweets']]);
-    if (isset($statuses[0])){
-      if(set_transient( 'swptc_latest_tweets', $statuses, $option['refresh_rate'] * HOUR_IN_SECONDS ))
-        echo 'transient set';
-      else
-        echo 'transient not set';
+    $statuses = $connection->get("statuses/user_timeline", ["screen_name" => $option['twitter_handle'], "count" => $option['num_tweets'], "exclude_replies" => true]);
+    if(isset($statuses->errors)){
+      echo $statuses->errors[0]->message;
     }else{
-      echo 'no statueses found';
+      if (isset($statuses[0])){
+        if(set_transient( 'swptc_latest_tweets', $statuses, $option['refresh_rate'] * HOUR_IN_SECONDS ))
+          echo 'Transient set.';
+        else
+          echo 'Transient not set.';
+      }else{
+        echo 'No statueses found.';
+      }
     }
   }else{
-    echo 'transient already set';
+    echo 'Transient already set.';
   }
 }else{
-  echo 'Please add settings';
+  echo 'Please add settings.';
 }
 die();
